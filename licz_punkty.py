4a357284e4e2f4272705939d470d61202f8fc80b
Radosław <98100532+RacuTheDreamer@users.noreply.github.com>
2023-12-10 13:29:50 +0100

    Update licz_punkty.py

--- licz_punkty.py
+++ licz_punkty.py
@@ -17,28 +17,41 @@ def policz_punkty_za_skok(odleglosc, wiatr, punkty_konstrukcyjne, noty):
         suma_punktow = 0
     return suma_punktow
 
-columns = ['ID_Zawodow', 'ID_Skoczka', 'ID_Skoczni', 'Odleglosc1', 'Odleglosc2']
-columns += [f'Nota{i}' for i in range(1, 11)]
-columns += ['Wiatr1', 'Wiatr2', 'Punkty']
+while True:
+    columns = ['ID_Zawodow', 'ID_Skoczka', 'ID_Skoczni', 'Odleglosc1', 'Odleglosc2']
+    columns += [f'Nota{i}' for i in range(1, 11)]
+    columns += ['Wiatr1', 'Wiatr2', 'Punkty']
 
-dane = []
+    dane = []
 
-# Umożliwienie użytkownikowi wprowadzenia punktu konstrukcyjnego
-punkty_konstrukcyjne = float(input("Podaj punkt konstrukcyjny: "))
+    # Umożliwienie użytkownikowi wprowadzenia punktu konstrukcyjnego
+    punkty_konstrukcyjne = float(input("Podaj punkt konstrukcyjny: "))
 
-# Umożliwienie użytkownikowi wprowadzenia danych
-odleglosc1 = float(input("Podaj odległość skoku 1: "))
-odleglosc2 = float(input("Podaj odległość skoku 2: "))
-noty = [float(input(f"Podaj notę {i}: ")) for i in range(1, 11)]
-wiatr1 = float(input("Podaj siłę wiatru dla skoku 1: "))
-wiatr2 = float(input("Podaj siłę wiatru dla skoku 2: "))
+    # Umożliwienie użytkownikowi wprowadzenia danych
+    odleglosc1 = float(input("Podaj odległość skoku 1: "))
+    odleglosc2 = float(input("Podaj odległość skoku 2: "))
+    noty = [float(input(f"Podaj notę {i}: ")) for i in range(1, 11)]
+    wiatr1 = float(input("Podaj siłę wiatru dla skoku 1: "))
+    wiatr2 = float(input("Podaj siłę wiatru dla skoku 2: "))
 
+    # Obliczanie punktów za skoki na podstawie podanych przez użytkownika danych
+    punkty_skok_1 = policz_punkty_za_skok(odleglosc1, wiatr1, punkty_konstrukcyjne, noty)
+    punkty_skok_2 = policz_punkty_za_skok(odleglosc2, wiatr2, punkty_konstrukcyjne, noty)
 
-# Obliczanie punktów za skoki na podstawie podanych przez użytkownika danych
-punkty_skok_1 = policz_punkty_za_skok(odleglosc1, wiatr1, punkty_konstrukcyjne, noty)
-punkty_skok_2 = policz_punkty_za_skok(odleglosc2, wiatr2, punkty_konstrukcyjne, noty)
+    # Wyświetlenie punktów
+    print(f"Punkty za skok 1: {punkty_skok_1}")
+    print(f"Punkty za skok 2: {punkty_skok_2}")
+    print(f"Suma punktów: {punkty_skok_1 + punkty_skok_2}")
 
-# Wyświetlenie punktów
-print(f"Punkty za skok 1: {punkty_skok_1}")
-print(f"Punkty za skok 2: {punkty_skok_2}")
-print(f"Suma punktów: {punkty_skok_1 + punkty_skok_2}")
+    # Wybór użytkownika
+    print("\nCzy chcesz powtórzyć operację?")
+    print("1. Tak")
+    print("2. Wyjdź z programu")
+    wybor = input("Twój wybór: ")
+
+    if wybor == "1":
+        continue  # Kontynuuj pętlę, aby wykonać operację ponownie
+    elif wybor == "2":
+        break  # Zakończ pętlę while, kończąc program
+    else:
+        print("Niepoprawny wybór. Spróbuj ponownie.")
